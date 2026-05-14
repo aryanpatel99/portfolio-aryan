db26d0d36b12a0f82b968306e1abc9b345920264
Aryan Patel <aryanpatel6215@gmail.com>
2026-05-14 14:08:36 +0530

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import 'react-activity-calendar/tooltips.css';
 
 import { ThemeProvider } from "./components/theme-provider"
 import { StrictMode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 )
